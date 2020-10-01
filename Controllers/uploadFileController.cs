5c8e85f5b1c42290f3f963897f44459a606c0997
Jose Garcia <josegarciaalvarez81@gmail.com>
2020-10-01 15:33:54 -0500

    modulo de UploadFiles Carga de archivos Ok

--- Controllers/uploadFileController.cs
+++ Controllers/uploadFileController.cs
@@ -1,6 +1,7 @@
 ﻿using CursoMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,12 +13,30 @@ namespace CursoMVC.Controllers
         // GET: uploadFile
         public ActionResult Index()
         {
+            if (TempData["Message"] != null)
+            {
+                ViewBag.Message = TempData["Message"];
+            }
             return View();
         }
 
+        [HttpPost]
         public ActionResult Save(FileViewModel model)
         {
-            return View();
+            string PathSite = Server.MapPath("~/");
+            string PathFile1 = Path.Combine(PathSite + "/Files/archivo1.pdf");
+            string PathFile2 = Path.Combine(PathSite + "/Files/archivo2.pdf");
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            model.File1.SaveAs(PathFile1);
+            model.File2.SaveAs(PathFile2);
+
+            @TempData["Message"] = "Se cargaron los Archivos";
+            return RedirectToAction("Index");
         }
     }
 }
