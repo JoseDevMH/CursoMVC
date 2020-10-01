08d736070f3863bff4a362544064439c46b091d7
Jose Garcia <josegarciaalvarez81@gmail.com>
2020-10-01 00:54:36 -0500

    diseño del formulario para cargar archivos v8 min 12:17

---
+++ Controllers/uploadFileController.cs
@@ -0,0 +1,23 @@
+﻿using CursoMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    public class uploadFileController : Controller
+    {
+        // GET: uploadFile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save(FileViewModel model)
+        {
+
+        }
+    }
+}
---
+++ Models/ViewModels/FileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.ViewModels
+{
+    public class FileViewModel
+    {
+        [Required]
+        [DisplayName ("Archivo 1")]
+        public HttpPostedFileBase File1 { get; set; }
+
+        [Required]
+        [DisplayName("Archivo 2")]
+        public HttpPostedFileBase File2 { get; set; }
+
+        [Required]
+        [DisplayName("Titulo")]
+        public string cadena { get; set; }
+    } 
+}
