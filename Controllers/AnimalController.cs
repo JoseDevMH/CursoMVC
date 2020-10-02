68d2ade6239c5b0f9b4bceb01f8617cb21792b3d
Jose Garcia <josegarciaalvarez81@gmail.com>
2020-10-02 16:39:45 -0500

    llenado de combos en cascada OK

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CursoMVC.Models;
 
 namespace CursoMVC.Controllers
 {
@@ -11,7 +12,41 @@ namespace CursoMVC.Controllers
         // GET: Animal
         public ActionResult Index()
         {
-            return View();
+            List<SelectListItem> lst = new List<SelectListItem>();
+            using (cursoMvcEntities db = new cursoMvcEntities())
+            {
+                lst = (from d in db.animalClass
+                       select new SelectListItem
+                       {
+                           Value = d.id.ToString(),
+                           Text = d.name
+                       }).ToList();
+            }
+            return View(lst);
+        }
+
+        [HttpGet]
+        public JsonResult Animal(int IdAnimalClass)
+        {
+            List<ElementJsonIntKey> lst = new List<ElementJsonIntKey>();
+            using (cursoMvcEntities db = new cursoMvcEntities()) 
+            {
+                lst = (from d in db.animal
+                       where d.idAnimalClass == IdAnimalClass
+                       select new ElementJsonIntKey
+                       {
+                           Value = d.id,
+                           Text = d.name
+                       }).ToList();
+            }
+
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+
+        public class ElementJsonIntKey
+        {
+            public int Value { get; set; }
+            public string Text { get; set; }
         }
     }
 }
